ac014e963e127ad6736994f8be4429e55ca1dc6d
ArindamBiswas-dev <arindambiswas616@gmail.com>
2021-05-09 08:59:39 +0530

    added the profile button

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,10 +1,16 @@
+import { Box, Flex } from '@chakra-ui/layout';
 import React from 'react';
 import AboutUser from './AboutUser';
 
 function SelfProfile() {
   return (
     <>
-      <AboutUser />
+      <Box bgColor="blue.700" h="130px" w="100%"></Box>
+      <Flex justify="center">
+        <Flex w={{ md: '85%', lg: '1024px' }} maxW="1024px" justify="center">
+          <AboutUser />
+        </Flex>
+      </Flex>
     </>
   );
 }
